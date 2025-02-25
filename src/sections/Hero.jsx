32b7283aff4c73c85bffc3b635e678c7ef0c0bbd
mohammad-nayem <nishanahmed13913@gmail.com>
2025-02-25 22:53:40 +0600

    Modified code

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -48,12 +48,9 @@ const Hero = () => {
                         className='object-contain relative z-10'
                     />
 
-                    <div className='flex sm:gap-6 gap-4 absolute -bottom-[5%] 
-                        sm:left-[10%] max-sm:px-6
-                    '>
+                    <div className='flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6'>
                         {
                             shoes.map((shoe) => {
-                                console.log(shoe);
                                 return (
                                     <div key={shoe}>
                                         <ShoeCard 
