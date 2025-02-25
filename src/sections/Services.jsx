b881045276d8016b6dd037f029936ac31e6a44b1
mohammad-nayem <nishanahmed13913@gmail.com>
2025-02-26 01:23:34 +0600

    Service section created

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { services } from '../constants';
+import ServiceCard from '../Components/ServiceCard';
 
 const Services = () => {
     return (
-        <div>
-            Services
-        </div>
+        <section className='max-container flex-center flex-wrap gap-9'>
+             {
+                services.map((service) => {
+                    return <ServiceCard key={service.label} {...service} />
+                })
+             }
+        </section>
     );
 };
 
