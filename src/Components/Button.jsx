f00d06b2bae5bddcb905f98e7aaa408fe73a1dda
mohammad-nayem <nishanahmed13913@gmail.com>
2025-02-26 15:31:34 +0600

    Subscribe section created

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
-const Button = ({label, iconURL}) => {
+const Button = ({label, iconURL, fullWidht, py}) => {
     return (
-       <button className='flex-center gap-2 cursor-pointer hover:bg-amber-900 px-7 py-4 leading-none font-montserrat text-lg text-white bg-coral-red rounded-full border border-coral-red'>
+       <button className={`flex-center gap-2 cursor-pointer hover:bg-amber-900 px-7 leading-none font-montserrat text-lg text-white bg-coral-red rounded-full border border-coral-red
+        ${fullWidht && 'w-full'}
+        ${py ? py : 'py-4'}
+       `}>
+
         {label} 
-        {
-            iconURL &&  <img src={iconURL} alt="Arrow" className='ml-2 rounded-full w-5 h-5' />
-        }
+        { iconURL &&  <img src={iconURL} alt="Arrow" className='ml-2 rounded-full w-5 h-5' /> }
        </button>
     );
 };
--- src/sections/Subscribe.jsx
+++ src/sections/Subscribe.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import Button from '../Components/Button';
 
 const Subscribe = () => {
     return (
-        <div>
-            Subscribe
-        </div>
+        <section id='contact-us' className='max-container flex-center max-lg:flex-col gap-10'>
+            <h1 className='text-4xl leading-[68px] lg:max-w-md font-palanquin font-bold'>Sign Up from <span className='text-coral-red'>Updates</span> & Newsletter
+            </h1>
+
+            <div className='lg:max-[40%] w-full flex-1 flex items-center max-sm:flex-col gap-5 p-2 sm:border sm:border-slate-300  rounded-full' >
+                <input type="text" id='subscribe' className='input' placeholder='subscribe@nike.com'/>
+                <div className='flex max-sm:justify-end items-center max-sm:w-full'>
+                    <Button label="Sign Up" fullWidht py='py-2.5' />
+                </div>
+            </div>
+        </section>
     );
 };
 
